b44fe2211f43f4b6264f6b3cb4eb94e1ed02f937
Hung mnk <thekingcrazycraft@gmail.com>
2024-07-27 11:58:31 +0700

    connect with excel

---
+++ src/ExcelIOManagement.java
@@ -0,0 +1,108 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.awt.Desktop;
+import java.io.File;
+import java.io.FileInputStream;
+
+public class ExcelIOManagement {
+	
+	private static final String INPUT_SHEET_NAME = "input";
+	
+	private static final String OUTPUT_SHEET_NAME = "output";
+	
+	private static final String EXCEL_DATA_PATH = "src/excel_resources/excel_data.xlsx";
+	
+	public static final String TEACHER_TABLE = "Giáo Viên";
+	
+	public static final String CLASS_TABLE = "Lớp";
+	
+	public static final String SPECIALITY_TABLE = "Môn Học";
+	
+	public static final String GROUP_TABLE = "Tổ";
+	
+	public static final String DAYS_TABLE = "Ngày Làm Việc";
+	
+	public static final String OTHERS_TABLE = "Khác";
+	
+	private FileInputStream input_file = null;
+	
+	private Workbook work_book;
+	
+	private static ExcelIOManagement instance;
+	
+	private ExcelIOManagement() {
+		
+		/* - bên trong try dường như các thuộc tính của đổi tượng từ lớp đó, hay cả của lớp đó 
+		 * đều không được nhận dạng, => cần phải khởi tạo thuộc tính mới
+		 * - có thể check nhiều try cùng lúc bằng việc phân tách bới ";" giữa các điều kiện cần check
+		 * */
+		
+		/* - FileOutputStream outputFile = new FileOutputStream(path);
+		 * - với cách khởi tạo trên thì nếu chưa tồn tại, thì java sẽ khởi tạo file đó theo địa chỉ đường dẫn
+		 * - Nếu đã có rồi thì toàn bộ dữ liệu muốn in ra file đó sẽ được java ghi đè hẳn file mới lên file cũ
+		 * - Nếu chỉ muốn thay thế dữ liệu mà không cần ghi đè file thì thêm điều kiện true sau path (điều kiện cho phép append hay không)
+		 * 
+		 * 
+		 * */
+		try(FileInputStream inputFile = new FileInputStream(EXCEL_DATA_PATH)){
+			
+			
+			if(inputFile != null) {
+				
+				this.input_file = inputFile;
+				
+				this.work_book = new XSSFWorkbook(inputFile);
+			}
+			
+			
+			
+			System.out.println("intitiate file and work_book successful !");
+			
+		} catch(IOException e) {
+			
+			e.printStackTrace();
+		}
+			
+		
+	}
+	
+	public static void main(String[] args) {
+		ExcelIOManagement.getInstance().print();
+	}
+	
+	public void print() {
+		Sheet sheet = this.work_book.getSheet(INPUT_SHEET_NAME);
+		
+		for(Row row : sheet) {
+			
+			boolean isStartedTable = false;
+			
+			Cell headerTable = row.getCell(0); 
+			
+			if( headerTable != null) {
+				System.out.println(headerTable.getStringCellValue());
+			}
+		}
+	}
+	
+	
+	
+	public static ExcelIOManagement getInstance() {
+		if(instance == null) {
+			instance = new ExcelIOManagement();
+		}
+		
+		return instance;
+	}
+	
+	
+}
